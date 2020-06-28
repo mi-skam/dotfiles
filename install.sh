f0ce076aca24dd3c179c4f1a15d0ae8c8e886a26
mi-skam <maksim.codes@mailbox.org>
2020-06-28 16:04:42 +0200

    install.sh understands now the release as first argument

--- install.sh
+++ install.sh
@@ -1,25 +1,30 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 # setup
 
 function on_wsl {
 	uname -a | grep -q microsoft 
 }
 
-read -p "Nix, Home-Manager release? (master, release-20.03,...) " answer
+function get_release {
+	read -p "Nix, Home-Manager release? (master, release-20.03,...) " answer
 
-while true
-do
-  case $answer in
-    master* ) release=master.tar.gz
-	      break;;
-    release-* ) release=$answer.tar.gz
-	      break;;
+	while true
+	do
+  	case $answer in
+    	master* ) release=master
+		      break;;
+    	release-* ) release=$answer
+		      break;;
 
-	* ) exit;;
-  esac
-done
-
+		* ) exit;;
+  	esac
+	done
+}
+# if there is more than one argument, then access $1
+[ "$#" -ge 1 ] && release=$1; shift || get_release
 
 # nix installieren (nicht auf NIXOS)
 if [ ! -f /etc/NIXOS ]; then
@@ -27,8 +32,9 @@ if [ ! -f /etc/NIXOS ]; then
 	source /home/$USER/.nix-profile/etc/profile.d/nix.sh
 	echo "READY: Nix is now in your path"
 fi
+
 # home-manager installieren
-nix-channel --add https://github.com/rycee/home-manager/archive/$release home-manager
+nix-channel --add https://github.com/rycee/home-manager/archive/$release.tar.gz home-manager
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
 nix-shell '<home-manager>' -A install
