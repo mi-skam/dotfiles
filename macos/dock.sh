fe9847865458356d0a069e67dfa5372d56e9bd5f
mi-skam <maksim@miskam.xyz>
2023-04-19 05:25:13 +0200

    Little maintenance updates:
    - added google-drive as a cask
    - added FQCNs to ansible package names
    - fixed some new lines and smaller issues
    - disabled configure_dock for localhost

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,18 +1,14 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/System/Applications/System Settings.app"
 dockutil --no-restart --add "/System/Applications/Utilities/Terminal.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Brave Browser.app"
+dockutil --no-restart --add "/Applications/Arc.app"
+dockutil --no-restart --add "/Applications/UTM.app"
 dockutil --no-restart --add "/System/Applications/Mail.app"
 dockutil --no-restart --add "/System/Applications/Calendar.app"
 dockutil --no-restart --add "/System/Applications/Books.app"
 dockutil --no-restart --add "/Applications/Reminders.app"
-dockutil --no-restart --add "~/Applications/Chrome Apps.localized/Element.app"
-dockutil --no-restart --add "/Applications/Notion.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Tidal.app"
 
